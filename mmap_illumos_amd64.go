edbd0c0f50805e2a84395ca20fb3efdbe2477523
Jan Mercl <0xjnml@gmail.com>
2022-12-04 11:32:15 +0100

    WIPS

--- mmap_illumos_amd64.go
+++ mmap_illumos_amd64.go
@@ -9,6 +9,7 @@ package memory // import "modernc.org/memory"
 import (
 	"os"
 	"syscall"
+	_ "unsafe"
 )
 
 const (
@@ -35,14 +36,17 @@ var (
 	osPageSize = os.Getpagesize()
 )
 
-func mmapSyscall(addr uintptr, length uintptr, prot int, flag int, fd int, pos int64) (ret uintptr, err error) {
-	r0, _, e1 := syscall.Syscall6(sys_MMAP, uintptr(addr), uintptr(length), uintptr(prot), uintptr(flag), uintptr(fd), uintptr(pos))
-	ret = uintptr(r0)
-	if e1 != 0 {
-		err = e1
-	}
-	return
-}
+//go:linkname mmapSyscall syscall.mmap
+func mmapSyscall(addr uintptr, length uintptr, prot int, flags int, fd int, offset int64) (xaddr uintptr, err error)
+
+// func mmapSyscall(addr uintptr, length uintptr, prot int, flag int, fd int, pos int64) (ret uintptr, err error) {
+// 	r0, _, e1 := syscall.Syscall6(sys_MMAP, uintptr(addr), uintptr(length), uintptr(prot), uintptr(flag), uintptr(fd), uintptr(pos))
+// 	ret = uintptr(r0)
+// 	if e1 != 0 {
+// 		err = e1
+// 	}
+// 	return
+// }
 
 func unmap(addr uintptr, size int) error {
 	_, _, errno := syscall.Syscall(sys_MUNMAP, addr, uintptr(size), 0)
